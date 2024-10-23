c4a7e8aaa698328c79017f32408e7ffbc4753e9f
bail0416 <bail0416@umn.edu>
2024-10-23 14:58:43 -0500

    Day 1 excercises

---
+++ Day 1 Coding Review/day_01_review/1_review/index.js
@@ -0,0 +1,50 @@
+// YOU CAN USE THIS FILE AS REFERENCE FOR SERVER DEVELOPMENT
+
+// include the express modules
+var express = require("express");
+
+// create an express application
+var app = express();
+const url = require('url');
+
+// helps in extracting the body portion of an incoming request stream
+var bodyparser = require('body-parser'); // this has been depricated, is now part of express...
+
+// fs module - provides an API for interacting with the file system
+var fs = require("fs");
+
+// helps in managing user sessions
+var session = require('express-session');
+
+
+// apply the body-parser middleware to all incoming requests
+app.use(bodyparser());
+
+// use express-session
+// in mremory session is sufficient for this assignment
+// app.use(session({
+//   secret: "csci4131secretkey",
+//   saveUninitialized: true,
+//   resave: false
+// }
+// ));
+
+// server listens on port 9007 for incoming connections
+app.listen(9007, () => console.log('Listening on port 9007!'));
+
+
+// function to return the welcome page
+app.get('/',function(req, res) {
+  console.log(__dirname)
+  res.sendFile(__dirname + '/static/html/review.html');
+});
+
+// middle ware to serve static files
+app.use('/static', express.static(__dirname + '/static'));
+
+
+// function to return the 404 message and error to client
+app.get('*', function(req, res) {
+  // add details
+  res.sendStatus(404);
+});
